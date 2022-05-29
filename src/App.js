ebf4ca27bb80cbb8201cc34654d46ede2d54c58c
ishajazmine <a.fredericksen@live.com.au>
2022-05-29 21:03:40 +0800

    second nav

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 //components
 import Nav from "./components/Nav/Nav"
+import Nav2 from "./components/Nav2/Nav2"
 
 
 //pages
@@ -20,15 +21,21 @@ function App() {
   return (
     <Router>
       <div>
-        <Nav/>
-          <Routes>
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/" element={<HomePage />} />
-            <Route path="/createaccount" element={<CreateAccountPage />} />
-            <Route path="/users/:id" element={<UserProfile/>} />
-            <Route path="/events/:id" element={<EventPage/>} />
-            <Route path="/events/id/signup" element={<SignUpPage/>} />
-          </Routes>
+        <div>
+          <Nav2>
+          </Nav2>
+        </div>
+        <div>
+          <Nav/>
+            <Routes>
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/" element={<HomePage />} />
+              <Route path="/createaccount" element={<CreateAccountPage />} />
+              <Route path="/users/:id" element={<UserProfile/>} />
+              <Route path="/events/:id" element={<EventPage/>} />
+              <Route path="/events/id/signup" element={<SignUpPage/>} />
+            </Routes>
+        </div>
       </div>
     </Router>
   );
---
+++ src/components/Nav2/Nav2.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import {Link, useLocation} from "react-router-dom";
+import "./Nav2.css";
+
+
+function Nav2() {
+    return (
+        <div className="header2">
+            <nav className="nav2">
+            <a href="https://shecodes.com.au/about/" target="_blank" rel="noopener noreferrer">About</a>
+            <a href="https://shecodes.com.au/frequently-asked-questions/" target="_blank" rel="noopener noreferrer">FAQ's</a>
+            <a href="https://shecodes.com.au/contact/" target="_blank" rel="noopener noreferrer">Contact</a>
+            <a href="https://shecodesaus.slack.com/archives/C9REWQFV2" target="_blank" rel="noopener noreferrer">Slack</a>
+            </nav>
+            {/* <div className="test">
+                <h1>
+                    <em>S</em>
+                    <em>H</em>
+                    <em class="planet left">E</em>
+                    <em>C</em>
+                    <em>O</em>
+                    <em>D</em>
+                    <em class="planet right">E</em>
+                    <em>S</em>
+                </h1>
+            </div> */}
+        </div>
+
+    )
+}
+
+export default Nav2;
