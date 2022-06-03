5540e566087eb5c9b6224ad2f6b145b4e6f5f49e
Jordan <63085677+jordantan2@users.noreply.github.com>
2022-06-03 21:42:44 +0800

    Invoice

---
+++ src/pages/InvoicePage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import InvoiceForm from "../components/Invoice/InvoiceForm";
+
+function InvoicePage( ) {
+    
+    return <InvoiceForm />;
+    
+}
+
+export default InvoicePage;
